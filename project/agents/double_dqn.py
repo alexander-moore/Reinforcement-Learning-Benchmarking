ae05d789294371d76ed28b9adbccaf4e5f64dc5d
Alexander Moore <31463017+moorea1@users.noreply.github.com>
2020-12-05 14:29:41 -0500

    double_dqn for mountrain

--- project/agents/double_dqn.py
+++ project/agents/double_dqn.py
@@ -38,9 +38,15 @@ class SampleAgent(Agent):
         # Load optimizer and loss function here
         self.optimizer = torch.optim.Adam(self.model.parameters(), lr=self.lr)
         self.criterion = torch.nn.SmoothL1Loss()
+        self.env = env
 
         # Any agent specific items done here
         self.target_model = copy.deepcopy(self.model)
+
+        if self.env.env.unwrapped.spec.id == 'Breakout-v0':
+        	self.game_shape = (32, 4, 84, 84)
+        elif self.env.env.unwrapped.spec.id == 'MountainCar-v0':
+        	self.game_shape = (32, 2)
 
     def make_action(self, observation, epsilon, test=True):
         """
@@ -57,12 +63,12 @@ class SampleAgent(Agent):
         # with torch.no_grad(): ?
         if np.random.rand(1) < 1. - epsilon or test:
             if self.env.env.unwrapped.spec.id == 'Breakout-v0':
-        		actions = self.model.forward(torch.from_numpy(np.array([observation]).transpose(0, 3, 1, 2)).float().to(self.device))
-        	elif self.env.env.unwrapped.spec.id == 'MountainCar-v0':
-        		#print(torch.from_numpy(np.array([observation])).float().to(self.device).shape)
-        		actions = self.model.forward(torch.from_numpy(np.array([observation])).float().to(self.device))
+        	    actions = self.model.forward(torch.from_numpy(np.array([observation]).reshape(self.game_shape)).float().to(self.device))
+            elif self.env.env.unwrapped.spec.id == 'MountainCar-v0':
+                #print(torch.from_numpy(np.array([observation])).float().to(self.device).shape)
+                actions = self.model.forward(torch.from_numpy(np.array([observation])).float().to(self.device))
         	#print(actions)
-        	action = torch.argmax(actions).item()
+            action = torch.argmax(actions).item()
         else:
             action = np.random.randint(self.env.env.action_space.n)
         
@@ -109,41 +115,53 @@ class SampleAgent(Agent):
         # YOUR IMPLEMENTATION HERE #
         # Reset our gradient
         self.optimizer.zero_grad()
+        
 
         # Get mini-batch for training
         training_states, training_actions, training_rewards, training_next_states, training_dones = self.replay_buffer()
+        states = torch.FloatTensor(training_states).to(self.device).reshape(self.game_shape)
+        actions = torch.LongTensor(training_actions).to(self.device)
+        rewards = torch.FloatTensor(training_rewards).to(self.device)
+        next_states = torch.FloatTensor(training_next_states).to(self.device).reshape(self.game_shape)
+        dones = torch.FloatTensor(training_dones).to(self.device)
 
         # Obtain our predictions
+        print(self.model, states.shape)
+        print(self.model(states).shape)
+        Q_S_A = self.model(states).gather(1, actions)
+        V_next = Variable(torch.zeros(batch_size, device = self.device))
 
-        if self.env.env.unwrapped.spec.id == 'Breakout-v0':
-        	predicted_Q = self.model(torch.from_numpy(training_states.transpose(0, 3, 1, 2)).float().to(self.device))
-        	predicted_Q_A = predicted_Q.gather(1, torch.from_numpy(training_actions).to(self.device).unsqueeze(1)).squeeze()
-        elif self.env.env.unwrapped.spec.id == 'MountainCar-v0':
-        	predicted_Q = self.model(torch.from_numpy(training_states).float().to(self.device))
-        	predicted_Q_A = predicted_Q.gather(1, torch.from_numpy(training_actions).type(torch.int64).to(self.device).unsqueeze(1)).squeeze()
+        next_S_A = self.model(next_states).gather(1, actions)
+        target_Q_S_A = rewards + (self.gamma * V_next)
+
+        loss = self.criterion(Q_S_A, target_Q_S_A)
+        self.optimizer.zero_grad()
+        loss.backward()
+        for param in self.model.parameters():
+        	param.grad.data.clam_(-1,1)
+
+        self.optimizer.step()
+
+        #predicted_Q = self.model(torch.from_numpy(training_states.transpose(0, 3, 1, 2)).float().to(self.device))
+        #predicted_Q_A = predicted_Q.gather(1, torch.from_numpy(training_actions).to(self.device).unsqueeze(1)).squeeze()
+
 
 
         # Obtain target Q values
-        with torch.no_grad():
-        	if self.env.env.unwrapped.spec.id == 'Breakout-v0':
-                target_Q = self.target_model(torch.from_numpy(training_next_states.transpose(0, 3, 1, 2)).float().to(self.device))
-                target_A = torch.argmax(self.model(torch.from_numpy(training_next_states.transpose(0, 3, 1, 2)).float().to(self.device)), dim=1)
-            elif self.env.env.unwrapped.spec.id == 'MountainCar-v0':
-                target_Q = self.target_model(torch.from_numpy(training_next_states).float().to(self.device))
-                target_A = torch.argmax(self.model(torch.from_numpy(training_next_states).float().to(self.device)), dim=1)
-
-            target_Q_A = target_Q.gather(1, target_A.unsqueeze(-1)).squeeze()
-            target_Q_A[training_dones] = 0.0
-            target_Q_A = torch.from_numpy(training_rewards).to(self.device) + (target_Q_A * self.gamma)
+        #target_Q = self.target_model(torch.from_numpy(training_next_states.transpose(0, 3, 1, 2)).float().to(self.device))
+        #target_A = torch.argmax(self.model(torch.from_numpy(training_next_states.transpose(0, 3, 1, 2)).float().to(self.device)), dim=1)
+        #target_Q_A = target_Q.gather(1, target_A.unsqueeze(-1)).squeeze()
+        #target_Q_A[training_dones] = 0.0
+        #target_Q_A = torch.from_numpy(training_rewards).to(self.device) + (target_Q_A * self.gamma)
 
         # Calculate our loss
-        loss = self.criterion(predicted_Q_A.float(), target_Q_A.float())
+        #loss = self.criterion(predicted_Q_A.float(), target_Q_A.float())
     
         # Perform gradient descent
-        loss.backward()
+        #loss.backward()
 
         # Update parameters
-        self.optimizer.step()
+        #self.optimizer.step()
 
         # Periodically update target model
         if step % self.target_update_steps == 0:
