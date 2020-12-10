b8640eb0db63a6f0eb374d4763400eacd54d6b3b
Brian Lewandowski <balewandowski@wpi.edu>
2020-12-09 22:19:49 -0500

    Updated mcc testing plot

--- project/results/plot_mcc.py
+++ project/results/plot_mcc.py
@@ -4,20 +4,30 @@ import pandas as pd
 import pickle
 import matplotlib.pyplot as plt
 import numpy as np
-import ipdb
 
 # Read in our test data
 reinforce = pd.read_csv('./ReinforceTEST-2020.12.08.10.50.57/4000_test_metrics.csv')
 actor_critic = pd.read_csv('./ContinuousMountainClimber-v0_actor_critic_2020.12.05.10.38.28/10000_test_metrics.csv')
-with open('./MountainCarContinuous_PPO_Results_pkl_15104.data', 'rb') as f:
-    ppo = pickle.load(f)
-ppo = np.repeat(ppo[3], 30)
+with open('./MountainCarContinuous_PPO_Results_pkl_10112.data', 'rb') as f:
+    ppo3 = pickle.load(f)
+ppo3 = ppo3[3]
+
+ppo = []
+for l in ppo3:
+   for item in l:
+       ppo.append(item)
+
+ppo = np.array(ppo)
+ppo_means = []
+
+for i in range(1,ppo.shape[0]):
+    ppo_means.append(np.mean(ppo[i-100:i]))
 
 # Plot test data
 plt.figure()
 plt.plot(reinforce.episode, reinforce.test_reward_average_last_100)
 plt.plot(actor_critic.episode, actor_critic.test_reward_average_last_100)
-plt.plot(range(len(ppo)), ppo)
+plt.plot(range(len(ppo_means)), ppo_means)
 plt.xlabel('Training Iterations')
 plt.ylabel('Mountain Car Continuous 100-Iteration Testing Reward')
 plt.title('Testing Rewards for Continuous Mountain Car')
